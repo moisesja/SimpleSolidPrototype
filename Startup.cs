dad11fedcee3d458ec1c2c60c1fe6c0315315a2d
Moises E. Jaramillo <moisesj@outlook.com>
2019-11-04 16:17:38 -0500

    Able to authenticate against Solid

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +20,14 @@ namespace SimpleSolidPrototype
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,12 +43,11 @@ namespace SimpleSolidPrototype
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthorization();
+            app.UseAuthorization();            
 
             app.UseEndpoints(endpoints =>
             {
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿async function showSolidLogin() {
+    
+    const popupUri = '/lib/solid-auth-client/dist-popup/popup.html';
+    const session = await solid.auth.popupLogin({ popupUri });
 
-// Write your JavaScript code.
+    if (session)
+        window.location.replace(homeRedirectUrl);
+}
+
+$(function ($) {
+
+    let authorizationToken = '';
+
+
+
+    $('#logout-button').hide();
+    $('#login-button').click(showSolidLogin);
+
+    solid.auth.trackSession(session => {
+
+        console.log('Moises say: Here\'s the session.', session);
+
+        const loggedIn = !!session;
+        
+        $('#login-button').toggle(!loggedIn);
+        $('#logout-button').toggle(loggedIn);
+
+        if (session) {
+
+            authorizationToken = session.authorization.id_token;
+            console.log('Token:', authorizationToken);
+            console.log('Web ID:', session.webId);
+
+        }
+        else {
+            console.log('Moises say: No session data.');
+        }
+    });
+
+    $('#logout-button').click(() => solid.auth.logout());    
+});
